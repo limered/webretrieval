995820221be18b71e46df23250ab0d9476c2d924
Emil <me@limered.de>
2014-07-21 18:35:29 +0200

    simple web server

--- ass2/src/uni/webret/ass02/Main.java
+++ ass2/src/uni/webret/ass02/Main.java
@@ -4,13 +4,20 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Indexer.index();
+		
 		try {
-			Searcher.search(null);
+			WebServer.start();
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+//		Indexer.index();
+//		try {
+//			Searcher.search(null);
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 	}
 
 }
