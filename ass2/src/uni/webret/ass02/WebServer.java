71e873cf2fbf7b3afc0d81ef062885be23948cb5
Emil <me@limered.de>
2014-07-21 20:03:11 +0200

    index.html serving

--- ass2/src/uni/webret/ass02/WebServer.java
+++ ass2/src/uni/webret/ass02/WebServer.java
@@ -1,6 +1,8 @@
 package uni.webret.ass02;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URL;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -8,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
 import org.eclipse.jetty.servlet.ServletHandler;
 
 public class WebServer {
@@ -16,10 +19,13 @@ public class WebServer {
 		
 		Server server = new Server(8080);
 		
-		ServletHandler handler = new ServletHandler();
-		server.setHandler(handler);
+		URL warURL = WebServer.class.getClassLoader().getResource("");
+		System.out.println(warURL.toExternalForm());
+		String warUrlString = warURL.toExternalForm();
+		WebAppContext webApp = new WebAppContext(warUrlString, "");
+		server.setHandler(webApp);
 		
-		handler.addServletWithMapping(HelloServlet.class, "/*");
+		webApp.addServlet(HelloServlet.class, "/html/q");
 		
 		server.start();
         server.join();
@@ -31,9 +37,18 @@ public class WebServer {
 		@Override
         protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
         {
-            response.setContentType("text/html");
-            response.setStatus(HttpServletResponse.SC_OK);
-            response.getWriter().println("<h1>Hello SimpleServlet</h1>");
+			if(request.getQueryString() == null){
+				response.setContentType("text/html");
+	            response.setStatus(HttpServletResponse.SC_OK);
+	            PrintWriter out = response.getWriter();
+	            out.append("hahahahahahahahaha</br> bjbajbajb");
+	            out.println("<h1>Your search:</h1>");
+	            out.println("<button type='submit' onclick='send();'>Your search:</button>");
+			}
+			
+			System.out.println(request.getQueryString());
+			
+
         }
 	}
 }
