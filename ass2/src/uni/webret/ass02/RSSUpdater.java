b336ff0e5ddf7522227f9ba47b82e4f525a3ab24
cemyabansu <cemyabansu@hotmail.com>
2014-07-22 22:03:43 +0300

    RSSUpdate without indexer
    RSSUpdate without indexer

--- ass2/src/uni/webret/ass02/RSSUpdater.java
+++ ass2/src/uni/webret/ass02/RSSUpdater.java
@@ -1,7 +1,16 @@
 package uni.webret.ass02;
 
+import it.sauronsoftware.feed4j.FeedIOException;
+import it.sauronsoftware.feed4j.FeedParser;
+import it.sauronsoftware.feed4j.FeedXMLParseException;
+import it.sauronsoftware.feed4j.UnsupportedFeedException;
+import it.sauronsoftware.feed4j.bean.Feed;
+import it.sauronsoftware.feed4j.bean.FeedItem;
+
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import org.json.simple.JSONObject;
 
@@ -13,19 +22,51 @@ public class RSSUpdater extends Thread {
 	public void run(){
 		while(running){
 			
-			JSONObject tweet = new JSONObject();
-			JSONObject contents = new JSONObject();
+			String urlList[] = new String[5];
+			urlList[0] = "http://www.gamespot.com/feeds/reviews/";
+			urlList[1] = "http://www.gamesradar.com/all-platforms/news/rss/";
 			
-			contents.put("time", "bla");
-			contents.put("user", "blo");
-			contents.put("content", "nana");
-			tweet.put("tweet", contents);
-			try {
-				PrintWriter out = new PrintWriter(path + "/tweet" + null + ".txt");
-				out.println(tweet.toString());
-				out.close();
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
+			for (int i = 0; i < 2; i++) {
+				URL url = null;
+				try {
+					url = new URL(urlList[0]);
+				} catch (MalformedURLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				Feed feed = null;
+				try {
+					feed = FeedParser.parse(url);
+				} catch (FeedIOException | FeedXMLParseException
+						| UnsupportedFeedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+
+				int items = feed.getItemCount();
+				for (int j = 0; j < items; j++) {
+					FeedItem item = feed.getItem(j);
+					
+					JSONObject rssfeed = new JSONObject();
+					JSONObject contents = new JSONObject();
+
+					contents.put("website", urlList[i]);
+					contents.put("title", item.getTitle());
+					contents.put("text", item.getDescriptionAsText());
+					contents.put("link",item.getLink());
+					rssfeed.put("rssfeed", contents);
+					
+					String id = String.format("%04d", j);
+					try {
+						PrintWriter out = new PrintWriter(path + "/rssfedd" + i+""+id
+								+ ".txt");
+						out.println(rssfeed.toString());
+						out.close();
+					} catch (FileNotFoundException e) {
+						e.printStackTrace();
+					}
+				}
+				
 			}
 				
 			try{
